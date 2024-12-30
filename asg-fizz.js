0f39d31a241e3a2898fdafa2d9fb4a97659cefc2
Shawoor Ayyoob <mohdshawoor@gmail.com>
2024-12-30 10:23:04 +0530

    Change

--- asg-fizz.js
+++ asg-fizz.js
@@ -2,7 +2,7 @@ let remainder;
 for (let i = 1; i <= 50; i++) {
     remainder = i % 3;
     if (remainder == 0) {
-        console.log("fizz");
+        console.log("fuz3");
     }
     else {
     console.log(i);
